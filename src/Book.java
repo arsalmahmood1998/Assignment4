4cb5dc8e8278b6f69766e8b09123fa65cd3a966c
arsalmahmood1998 <68080848+arsalmahmood1998@users.noreply.github.com>
2020-08-26 21:07:02 +0500

    formatted

--- src/Book.java
+++ src/Book.java
@@ -10,23 +10,22 @@ public class Book {
 	public void isBookBorrowed(String bookTitle) {
 		if(!borrowed) {
 			System.out.println(bookTitle+" is availale in our library");
-		}
+			}
 		else {
 			System.out.println(bookTitle+" is already borrowed");
+			}
 		}
-	}
 	public void borrowBook(String bookTitle) {
 		if(!borrowed) {
-	System.out.println("You successfully borrowed "+ bookTitle);
-	borrowed=true;
-		}
+			System.out.println("You successfully borrowed "+ bookTitle);
+			borrowed=true;
+			}
 		else {
 			System.out.println(bookTitle+" is already borrowed");
+			}
 		}
-	}
 	public void returned(String bookTitle) {
 		borrowed=false;
 		System.out.println("You successfully returned "+ bookTitle);
-	}
-	
+		}	
 }
